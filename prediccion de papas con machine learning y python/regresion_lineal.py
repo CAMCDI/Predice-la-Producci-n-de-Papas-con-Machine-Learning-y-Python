4e7f04e0a938628ed0e89281650ecd17e297a21e
Cesar Antonio mtz <92832278+CAMCDI@users.noreply.github.com>
2025-03-07 23:28:32 -0600

    Update regresion_lineal.py

--- prediccion de papas con machine learning y python/regresion_lineal.py
+++ prediccion de papas con machine learning y python/regresion_lineal.py
@@ -15,7 +15,7 @@ fertilizante = np.random.uniform(0,100,n)
 
 #definir una relacion lineal con ruido aleatorio para la cantidad de papas
 #suponemos que por cada kg de fertilizante obtendremos 0.8 toneladas
-# y agregamos un oco de ruido normal para simular variedad normaal 
+# y agregamos un poco de ruido normal para simular variedad normaal 
 papas = 0.8 * fertilizante + np.random.normal(0,5,n) + 8 # intersecto en 8 
 
 df = pd.DataFrame ({
@@ -26,7 +26,7 @@ df = pd.DataFrame ({
 X = df[['Fertilizante' (kg)]].values
 Y = df['Papas (toneladas)'].values
 
-#separar los datos de netrenamiento y prueba
+#separar los datos de tratamiento y prueba
 X_train, X_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state=0)
 
 #definir modelo y entrenarlo
