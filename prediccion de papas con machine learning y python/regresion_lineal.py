df920c53437152f0c6498d256b53cdac4b0da591
Cesar Antonio mtz <92832278+CAMCDI@users.noreply.github.com>
2025-03-10 21:45:39 -0600

    Update regresion_lineal.py

--- prediccion de papas con machine learning y python/regresion_lineal.py
+++ prediccion de papas con machine learning y python/regresion_lineal.py
@@ -4,6 +4,7 @@ import numpy as np
 from pandasgui import show
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_squared_error
 
 
 # crear datos ficticios
@@ -36,7 +37,47 @@ regressor.fit(X_train,y_train)
 #hacer la prediccion 
 y_pred = regressor.predict(X_test)
 
+# calcular b0 y b1
+b1 = regressor.coef_[0]
+b0 = regressor.intercept_
+
+print(f"""
+    b1: {b1}
+    b0) {b0}
+""")
+
+# calcular mse. rmse, r^2
+mse = mean_squared_error(y_test,y_pred)
+rmse = np.sqrt(mse)
+r2 = r2_score(y_test, y_pred)
+
+print(f"""
+    mse: {mse}
+    rmse: {rmse}
+    r2: {r2}
+""")
+
+
 plt.figure(figsize=(10,6))
+
+# scatter plot del set de entrenamiento 
+
+plt.scatter(X_train,y_train, color="blue", label="datos de entrenamiento")
+
+# scatter plot para el set de prueba
+plt.scatter(X_test,y_test, color="green", label="Datos de prueba")
+
+plt.plot(X_train,regressor.predict(X_train), color="read", lable="linea de regresion")
+
+plt.title("regresion lineal: fertilizante vs produccion de papas")
+
+plt.xlabel("fertilizante (kg)")
+plt.ylabel("papas (toneladas)")
+plt.legend()
+
+plt.show()
+
+
 
 
 
